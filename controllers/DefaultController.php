83a2bab1d1d3de257a527155ca231c7a94f6c925
essedi <dario@essediconsulting.com>
2017-06-05 12:20:41 +0200

    Before & After login hooks

--- controllers/DefaultController.php
+++ controllers/DefaultController.php
@@ -5,7 +5,7 @@ namespace filsh\yii2\oauth2server\controllers;
 use Yii;
 use yii\helpers\ArrayHelper;
 use filsh\yii2\oauth2server\filters\ErrorToExceptionFilter;
-use api\models\User;
+use api\common\helpers\CommonHelper;
 class DefaultController extends \yii\rest\Controller
 {
     /**
@@ -22,27 +22,11 @@ class DefaultController extends \yii\rest\Controller
     
     public function actionToken()
     {
-        //The first time a user logs in, he doesn't know his client_id, client_secret or ims_id
-        //so we are going to default all those params to the user's account.
-        if($_POST['grant_type'] === 'password' && (!isset($_POST['client_id']) || !$_POST['client_id']) && User::findByEmail($_POST['username']))
-        {
-            $usr = User::findByEmail($_POST['username']);
-            //Get ims id
-            $ims = \api\common\models\Ims::findByUserid($usr['id']);
-            $oauthClient = \filsh\yii2\oauth2server\models\OauthClients::findOne(['user_id'=>$usr['id']]);
-            if($ims && $oauthClient)
-            {
-                $_POST['client_id'] = $oauthClient->client_id;
-                $_POST['client_secret'] = $oauthClient->client_secret;
-                $_POST['ims_id'] = $ims['id'];
-            }
-        }
-        
-        
+        CommonHelper::beforeLogin();
         $server = $this->module->getServer();
         $request = $this->module->getRequest();
         $response = $server->handleTokenRequest($request);
-        
+        CommonHelper::afterLogin($loginResult);
         return $response->getParameters();
     }
 }
